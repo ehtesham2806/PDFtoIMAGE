0477cbb573956b1b5b8e38c10d7907513165a2a4
Ehtesham <ealam@anteriad.com>
2025-03-11 11:37:14 +0530

    Changes Dp color

--- pdf-image-extractor/dropdowns.py
+++ pdf-image-extractor/dropdowns.py
@@ -3,372 +3,372 @@ DROPDOWN_OPTIONS = {
         "name": "The HR Empire (THRE)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#FA4D09"  
+        "bgcolor": "#4C4084"
     },
     "techversions": {
         "name": "TechVersions (TV)", 
         "width": 670, 
         "height": 900,
-        "bgcolor": "#0273ff"  
+        "bgcolor": "#12A555"
     },
     "businessmediums": {
         "name": "Business Mediums (BM)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#0a2f41"  
+        "bgcolor": "#333333"
     },
     "cloud-tech-alert": {
         "name": "CloudTech Alert (CTA)", 
         "width": 585, 
         "height": 450,
-        "bgcolor": "#FF5733"  
+        "bgcolor": "#121852"
     },
     "thebusinessinnovations": {
         "name": "The Business Innovations (TBI)", 
         "width": 755, 
         "height": 360,
-        "bgcolor": "#33FF57"  
+        "bgcolor": "#32ABDA"
     },
     "economicmatter": {
         "name": "Economic Matter (EM)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#3357FF"  
+        "bgcolor": "#003466"
     },
     "hrinterests": {
         "name": "HR Interests (HRI)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#FF33A1"  
+        "bgcolor": "#2C3E50"
     },
     "cxomatters": {
         "name": "CXO Matters (CXOM)", 
         "width": 600, 
         "height": 440,
-        "bgcolor": "#A133FF"  
+        "bgcolor": "#FE000A"
     },
     "theroadtoprofit": {
         "name": "The Road to Profit (TRtP)", 
         "width": 600, 
         "height": 460,
-        "bgcolor": "#33FFF5"  
+        "bgcolor": "#EF7F1A"
     },
     "techalertlive": {
         "name": "Tech Alert Live (TAL)", 
         "width": 755, 
         "height": 360,
-        "bgcolor": "#F5FF33"  
+        "bgcolor": "#092F40"
     },
     "techmasternode": {
         "name": "TechMaster Node (TMN)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#FF8C33"  
+        "bgcolor": "#264653"
     },
     "thetechaffair": {
         "name": "The Tech Affair (TTA)", 
         "width": 780, 
         "height": 370,
-        "bgcolor": "#8C33FF"  
+        "bgcolor": "#FC4C02"
     },
     "techwebtrends": {
         "name": "TechWeb Trends (TWT)", 
         "width": 480, 
         "height": 360,
-        "bgcolor": "#33FF8C"  
+        "bgcolor": "#052F85"
     },
     "digiinfluence": {
         "name": "Digi Influence (DI)", 
         "width": 600, 
         "height": 440,
-        "bgcolor": "#FF338C"  
+        "bgcolor": "#09094F"
     },
     "thebusinesscover": {
         "name": "The BusinessCover (TBC)", 
         "width": 640, 
         "height": 480,
-        "bgcolor": "#338CFF"  
+        "bgcolor": "#F04636"
     },
     "thesalesmark": {
         "name": "The Salesmark (TSm)", 
         "width": 600, 
         "height": 440,
-        "bgcolor": "#FF5733"  
+        "bgcolor": "#C53F45"
     },
     "bizversions": {
         "name": "BizVersions (BV)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#33FF57"  
+        "bgcolor": "#1E5631"
     },
     "protechinsights": {
         "name": "ProTech Insights (PTI)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#3357FF"  
+        "bgcolor": "#EB6228"
     },
     "deltapunch": {
         "name": "Delta Punch (DP)", 
         "width": 555, 
         "height": 406,
-        "bgcolor": "#FF33A1"  
+        "bgcolor": "#0617b7"
     },
     "financepulsedaily": {
         "name": "Finance Pulse Daily (FPD)", 
         "width": 755, 
         "height": 360,
-        "bgcolor": "#A133FF"  
+        "bgcolor": "#98BC62"
     },
     "leadmarketwise": {
         "name": "Lead Marketwise (LMw)", 
         "width": 700, 
         "height": 300,
-        "bgcolor": "#33FFF5"  
+        "bgcolor": "#00A562"
     },
     "thegrowthinsights": {
         "name": "The Growth Insights (TGI)", 
         "width": 760, 
         "height": 570,
-        "bgcolor": "#F5FF33"  
+        "bgcolor": "#01BAB4"
     },
     "techmasterpros": {
         "name": "TechMaster Pros (TMP)", 
         "width": 480, 
         "height": 360,
-        "bgcolor": "#FF8C33"  
+        "bgcolor": "#000066"
     },
     "techpulsedaily": {
         "name": "TechPulse Daily (TPD)", 
         "width": 600, 
         "height": 439,
-        "bgcolor": "#8C33FF"  
+        "bgcolor": "#7D0101"
     },
     "businessupdates360": {
         "name": "Business Updates 360 (BU360)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#33FF8C"  
+        "bgcolor": "#04839A"
     },
     "thebestofblockchain": {
         "name": "The Best of Blockchain (TBoB)", 
         "width": 600, 
         "height": 460,
-        "bgcolor": "#FF338C"  
+        "bgcolor": "#892FC8"
     },
     "regulatorycompliancenews": {
         "name": "Regulatory Compliance News (RCN)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#338CFF"  
+        "bgcolor": "#32c431"
     },
     "thetechnobytes": {
         "name": "The Techno Bytes (TTB)", 
         "width": 760, 
         "height": 360,
-        "bgcolor": "#FF5733"  
+        "bgcolor": "#B666A5"
     },
     "alltechinsights": {
         "name": "AllTech Insights (ATI)", 
         "width": 555, 
         "height": 406,
-        "bgcolor": "#33FF57"  
+        "bgcolor": "#A1A9AC"
     },
     "globalsupplychainnews": {
         "name": "Global Supplychain News (GScN)", 
         "width": 555, 
         "height": 406,
-        "bgcolor": "#3357FF"  
+        "bgcolor": "#006996"
     },
     "learningtechedu": {
         "name": "LearningTech Edu (LTE)", 
         "width": 600, 
         "height": 450,
-        "bgcolor": "#FF33A1"  
+        "bgcolor": "#333333"
     },
     "growthratefinder": {
         "name": "Growth Rate Finder (GRF)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#A133FF"  
+        "bgcolor": "#FE5C36"
     },
     "healthcarebusinesssolution": {
         "name": "Healthcare Business Solutions (HBS)", 
         "width": 600, 
         "height": 439,
-        "bgcolor": "#33FFF5"  
+        "bgcolor": "#3cb0e3"
     },
     "hrtrendsdaily": {
         "name": "HR Trends Daily (HRTD)", 
         "width": 600, 
         "height": 460,
-        "bgcolor": "#F5FF33"  
+        "bgcolor": "#1D9F95"
     },
     "hrmecosystem": {
         "name": "HRM Ecosystem (HRME)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#FF8C33"  
+        "bgcolor": "#3081fa"
     },
     "hrpulsedaily": {
         "name": "HR Pulse Daily (HRPD)", 
         "width": 753, 
         "height": 565,
-        "bgcolor": "#8C33FF"  
+        "bgcolor": "#092f41"
     },
     "livingsights": {
         "name": "Livingsights (Ls)", 
         "width": 555, 
         "height": 406,
-        "bgcolor": "#33FF8C"  
+        "bgcolor": "#176B97"
     },
     "thehrmedium": {
         "name": "The HR Medium (THRM)", 
         "width": 632, 
         "height": 486,
-        "bgcolor": "#FF338C"  
+        "bgcolor": "#20E4E2"
     },
     "thetechnodigest": {
         "name": "The Techno Digest (TTD)", 
         "width": 640, 
         "height": 480,
-        "bgcolor": "#338CFF"  
+        "bgcolor": "#11B7EB"
     },
     "bestofbusinesstoday": {
         "name": "Best of Business Today (BoBT)", 
         "width": 768, 
         "height": 367,
-        "bgcolor": "#FF5733"  
+        "bgcolor": "#F76C5A"
     },
     "businessnarratives": {
         "name": "Business Narratives (BN)", 
         "width": 630, 
         "height": 485,
-        "bgcolor": "#33FF57"  
+        "bgcolor": "#393185"
     },
     "businessproinsights": {
         "name": "Business Proinsights (BPi)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#3357FF"  
+        "bgcolor": "#061D61"
     },
     "business-target": {
         "name": "Business Target (BT)", 
         "width": 555, 
         "height": 406,
-        "bgcolor": "#FF33A1"  
+        "bgcolor": "#000000"
     },
     "thebusinessleaderinsights": {
         "name": "Business Leader Insights (BLI)", 
         "width": 620, 
         "height": 477,
-        "bgcolor": "#A133FF"  
+        "bgcolor": "#243666"
     },
     "businessstoriestoday": {
         "name": "Business Stories Today (BST)", 
         "width": 768, 
         "height": 367,
-        "bgcolor": "#33FFF5"  
+        "bgcolor": "#37A987"
     },
     "financeeveryhour": {
         "name": "Finance Every Hour (FEH)", 
         "width": 650, 
         "height": 475,
-        "bgcolor": "#F5FF33"  
+        "bgcolor": "#37A987"
     },
     "infobytesdaily": {
         "name": "InfoBytes Daily (IBD)", 
         "width": 755, 
         "height": 360,
-        "bgcolor": "#FF8C33"  
+        "bgcolor": "#1596BC"
     },
     "infowatchdaily": {
         "name": "Infowatch Daily (IwD)", 
         "width": 632, 
         "height": 486,
-        "bgcolor": "#8C33FF"  
+        "bgcolor": "#00455e"
     },
     "marketinsightstoday": {
         "name": "Market Insights Today (MIT)", 
         "width": 600, 
         "height": 460,
-        "bgcolor": "#33FF8C"  
+        "bgcolor": "#33679a"
     },
     "salesnewton": {
         "name": "Sales Newton (SN)", 
         "width": 753, 
         "height": 565,
-        "bgcolor": "#FF338C"  
+        "bgcolor": "#002A4A"
     },
     "salesmarketscoop": {
         "name": "SalesMarket Scoop (SMS)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#338CFF"  
+        "bgcolor": "#172450"
     },
     "techinsightstoday": {
         "name": "Techinsights Today (TiT)", 
         "width": 650, 
         "height": 475,
-        "bgcolor": "#FF5733"  
+        "bgcolor": "#37A987"
     },
     "technobeatdaily": {
         "name": "Technobeat Daily (TbD)", 
         "width": 480, 
         "height": 360,
-        "bgcolor": "#33FF57"  
+        "bgcolor": "#262261"
     },
     "theeconomydigest": {
         "name": "The Economy Digest (TED)", 
         "width": 600, 
         "height": 440,
-        "bgcolor": "#3357FF"  
+        "bgcolor": "#28274C"
     },
     "thefinancesreport": {
         "name": "The Finances Report (TFR)", 
         "width": 620, 
         "height": 477,
-        "bgcolor": "#FF33A1"  
+        "bgcolor": "#464646"
     },
     "thesalesinsights": {
         "name": "The Sales Insights (TSI)", 
         "width": 555, 
         "height": 406,
-        "bgcolor": "#A133FF"  
+        "bgcolor": "#011B34"
     },
     "theuniversalinsights": {
         "name": "The Universal Insights (TUI)", 
         "width": 650, 
         "height": 475,
-        "bgcolor": "#33FFF5"  
+        "bgcolor": "#37A987"
     },
     "worldbusinesshour": {
         "name": "World Business Hour (WBH)", 
         "width": 555, 
         "height": 406,
-        "bgcolor": "#F5FF33"  
+        "bgcolor": "#00D6C1"
     },
     "insightsliving": {
         "name": "Insights Living (IL)", 
         "width": 400, 
         "height": 310,
-        "bgcolor": "#FF8C33"  
+        "bgcolor": "#11B7EB"
     },
     "thetechnologysquare": {
         "name": "The Technology Square (TTS)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#8C33FF"  
+        "bgcolor": "#DD2A1B"
     },
     "healthcareinsightstoday": {
         "name": "Healthcare Insights Today (HIT)", 
         "width": 650, 
         "height": 500,
-        "bgcolor": "#33FF8C"  
+        "bgcolor": "#319082"
     },
     "protechempire": {
         "name": "Protech Empire (PtE)", 
         "width": 585, 
         "height": 540,
-        "bgcolor": "#FF338C"  
+        "bgcolor": "#37A987"
     }
 }
